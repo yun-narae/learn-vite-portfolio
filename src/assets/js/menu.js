c595b4f2926733a4dd2c68234f094f35c7edfba1
seulbinim <skfo0827@naver.com>
2025-01-22 13:12:28 +0900

    STEP01) 메뉴 열고 닫기

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,9 @@
 export function menu() {
-    console.log("menu")
+    const headerToggle = document.getElementById("headerToggle");
+    const headerNav = document.getElementById("headerNav");
+
+    // 메뉴 토글 버튼 클릭 시 메뉴 열고 닫기
+    headerToggle.addEventListener("click", () => {
+        headerNav.classList.toggle("show");
+    });
 };
