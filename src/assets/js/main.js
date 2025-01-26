190925061e4db7a837038f53d067e026362b2ed0
seulbinim <skfo0827@naver.com>
2025-01-26 23:14:22 +0900

    emailjs라이브러리 설치

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,10 +2,17 @@ import { menu } from "./menu.js";
 import { port } from "./port.js";
 import { link } from "./link.js";
 import { smooth } from "./smooth.js";
+import { email } from "./email.js";
 
 window.addEventListener("load", function(){
     menu();
     port();
     link();
     smooth();
+    
+    // 폼의 submit 이벤트 리스너를 추가하여 이메일 전송 함수 연결
+    const form = document.getElementById("contact-form");
+    if (form) {
+        form.addEventListener("submit", email);  // submit 이벤트에 email 함수 연결
+    }
 });
