d5ace6f55cd83f1d51db0d3da7c819f46a4d2b0c
seulbinim <skfo0827@naver.com>
2025-01-27 13:09:48 +0900

    Port 가로 스크롤 화살표 추가

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -1,7 +1,11 @@
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
+
 export function port() {
+    const arrow = document.querySelector('.arrow');
+    gsap.to(arrow,{ x:10, repeat:-1, yoyo:true });
+
     gsap.registerPlugin(ScrollTrigger);
     const horSection = gsap.utils.toArray(".port__item");
 
